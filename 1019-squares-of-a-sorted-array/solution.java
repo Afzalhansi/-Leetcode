df4066accefde76eff1426f7efd4a57ded1646d9
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-16 12:09:05 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 47.1 MB (76.52%)

--- 1019-squares-of-a-sorted-array/solution.java
+++ 1019-squares-of-a-sorted-array/solution.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        
+
         int left = 0;
         int right = nums.length - 1;
-        int index = nums.length - 1;
         int[] result = new int[nums.length];
+        int leftsquare = 0;
+        int rightsquare = 0;
+        int index = nums.length - 1;
 
         while(left <= right){
-            int leftsquare = nums[left]*nums[left];
-            int rightsquare = nums[right]*nums[right];
-
-            if(leftsquare >= rightsquare){
+            leftsquare = nums[left] * nums[left];
+            rightsquare = nums[right] * nums[right];
+            if(leftsquare > rightsquare){
                 result[index] = leftsquare;
                 left++;
-            }else {
+            }else{
                 result[index] = rightsquare;
                 right--;
             }
             index--;
         }
-
-        return result;
+    return result;
     }
 }
