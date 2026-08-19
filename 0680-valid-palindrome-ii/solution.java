f8dd3e6feaaab208d255311b4a04c16e09df1d02
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-19 13:15:29 +0000

    Sync LeetCode submission Runtime - 4 ms (99.07%), Memory - 47.9 MB (12.40%)

--- 0680-valid-palindrome-ii/solution.java
+++ 0680-valid-palindrome-ii/solution.java
@@ -1,28 +1,28 @@
 class Solution {
+
     private boolean isPalindrome(String s, int left, int right){
-        
+
         while(left < right){
             if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            left++;
             right--;
+            left++;
         }
-        return true;
+         return true;
     }
     public boolean validPalindrome(String s) {
-
+        
         int left = 0;
         int right = s.length() - 1;
 
         while(left < right){
-
             if(s.charAt(left) != s.charAt(right)){
-                return isPalindrome(s,left + 1, right) || isPalindrome(s, left, right - 1);
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
             left++;
             right--;
         }
-        return true;
+    return true;    
     }
 }
