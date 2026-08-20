b0a37f93727a1173a35715a4e41e9dd801e36b6d
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-20 14:28:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (42.75%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -5,13 +5,12 @@ class Solution {
         int mid = 0;
         int high = nums.length - 1;
 
-
         while(mid <= high){
+
             if(nums[mid] == 0){
                 int temp = nums[low];
                 nums[low] = nums[mid];
                 nums[mid] = temp;
-
                 low++;
                 mid++;
             }else if(nums[mid] == 1){
@@ -21,8 +20,8 @@ class Solution {
                 nums[mid] = nums[high];
                 nums[high] = temp;
                 high--;
-            }
+            }      
         }
-    return;
+        return;
     }
 }
