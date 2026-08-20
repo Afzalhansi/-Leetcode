6d6c947d8d7fab42684bdb0ad00ee8174fb01968
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-20 07:57:40 +0000

    Sync LeetCode submission Runtime - 3 ms (57.52%), Memory - 44.5 MB (54.41%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        int left = 0;
-        int right = s.length() - 1;
-        String s1 = s.toLowerCase();
-       
-        while(left < right){  
-            
-            if(!Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-                continue;
-            }
-            if(!Character.isLetterOrDigit(s.charAt(right))){
+       int left = 0;
+       int right = s.length() - 1;
+       String s1 = s.toLowerCase();
+
+    while(left < right){
+
+            while(right >=0 && !Character.isLetterOrDigit(s1.charAt(right))){
                 right--;
-                continue;
             }
-        
+
+            while(left < s1.length() && !Character.isLetterOrDigit(s1.charAt(left))){
+                left++;
+            }
+
+            if (left >= right) {
+                return true;
+            }
+
             if(s1.charAt(left) != s1.charAt(right)){
-                return false;
-            }
+                return false; 
+            }   
             left++;
-            right--;
+            right--; 
         }
         return true;
     }
