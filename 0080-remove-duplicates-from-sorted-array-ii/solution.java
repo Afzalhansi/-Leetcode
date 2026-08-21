0936b22a44c57850a2f3b676481eec05bec6b8b6
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-21 07:31:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.7 MB (70.35%)

--- 0080-remove-duplicates-from-sorted-array-ii/solution.java
+++ 0080-remove-duplicates-from-sorted-array-ii/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-        if(nums.length ==0 || nums.length == 1){
-            return nums.length;
-        }
+       int slow = 2;
 
-        int slow = 2;
-        for(int fast = 2; fast< nums.length; fast++){
-            if(nums[fast] != nums[slow-2]){
+       for(int fast = 2; fast < nums.length; fast++){
+            if(nums[fast] == nums[slow - 2]){
+                continue;
+            }else{
                 nums[slow] = nums[fast];
                 slow++;
             }
-        }
-    return slow;
+       }
+       return slow;
     }
 }
