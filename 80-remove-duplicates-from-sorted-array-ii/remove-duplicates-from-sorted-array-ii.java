b544cf1c7ca804a34241401e0d7391863740e1a6
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-21 13:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 48.7 MB (70.47%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+       int slow = 2;
+
+       for(int fast = 2; fast < nums.length; fast++){
+            if(nums[fast] == nums[slow - 2]){
+                continue;
+            }else{
+                nums[slow] = nums[fast];
+                slow++;
+            }
+       }
+       return slow;
+    }
+}
