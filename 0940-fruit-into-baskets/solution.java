dbc979a5ee1451da8e9ce7e653612b99ea440e82
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-08-29 09:40:10 +0000

    Sync LeetCode submission Runtime - 55 ms (43.26%), Memory - 70.3 MB (74.52%)

--- 0940-fruit-into-baskets/solution.java
+++ 0940-fruit-into-baskets/solution.java
@@ -1,41 +1,23 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-
         int left = 0;
-        int right = 0;
+        int maxlength = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int maxlength = 0;
+        for(int right =0; right < fruits.length; right++){
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
 
-        while (right < fruits.length) {
+            while(map.size() > 2){
+                map.put(fruits[left], map.get(fruits[left]) - 1);
 
-            // Add current fruit
-            map.put(
-                fruits[right],
-                map.getOrDefault(fruits[right], 0) + 1
-            );
-
-            // Shrink window if we have more than 2 fruit types
-            while (map.size() > 2) {
-
-                int fruit = fruits[left];
-
-                map.put(fruit, map.get(fruit) - 1);
-
-                if (map.get(fruit) == 0) {
-                    map.remove(fruit);
+                if (map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
                 }
-
                 left++;
             }
-
-            // Current window is valid
             maxlength = Math.max(maxlength, right - left + 1);
-
-            right++;
         }
-
-        return maxlength;
+     return maxlength;   
     }
 }
