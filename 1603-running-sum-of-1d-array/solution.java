e2338bce201f166796c65d877b308b3b38d31da3
Abdul Basit Hansi <abdulhansi667@gmail.com>
2026-09-09 09:45:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.5 MB (17.06%)

--- 1603-running-sum-of-1d-array/solution.java
+++ 1603-running-sum-of-1d-array/solution.java
@@ -1,10 +1,8 @@
     class Solution {
         public int[] runningSum(int[] nums) {
-            int prefixsum = 0;
-
-            for(int i = 0; i < nums.length; i++){
-                prefixsum = prefixsum + nums[i];
-                nums[i] = prefixsum;
+            
+            for(int i =1; i < nums.length; i++){
+               nums[i]=nums[i] + nums[i - 1];
             }
             return nums;
         }
